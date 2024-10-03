04ba8a098edbe21fabcc526f7ea5b30924b0dc56
adepuu <ade.santito@gmail.com>
2024-10-03 10:50:22 +0700

    feat: add fetch product api call

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,36 @@
-import {FC} from "react";
+import {
+  FC,
+  useEffect,
+  useState
+} from "react";
 import Card from "../component/Card";
 import CheckoutCta from "../component/CheckoutCta";
 import Header from "../component/Header";
-import {PRODUCT} from "../constants/products.ts";
+import {Product} from "../types/product";
 
 const Home: FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch the data from an API Endpoint and wait until request is done
+        const resp = await fetch("http://localhost:8080/products");
+        // When request is done, parse the JSON data as Product[] and set the state
+        const data = await resp.json() as Product[];
+        setProducts(data);
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    fetchData();
+    console.log("Fetch data callled")
+  }, []);
+  
   return (
     <div className="grid grid-cols-2 gap-4 px-4 pt-20 relative">
       <Header />
-      {PRODUCT.map((product, index) => (
+      {products.map((product, index) => (
         <Card key={index} {...product} />
       ))}
       <CheckoutCta />
--- src/types/product.d.ts
+++ src/types/product.d.ts
@@ -1,5 +1,19 @@
-type Product = {
-  name: string;
-  price: number;
-  stock: number;
-};
+export interface Product {
+  price: number
+  weight: number
+  name: string
+  category: string
+  imageUrl: string
+  metadata: Metadata
+  id: string
+}
+
+export interface Metadata {
+  unit: string
+  weight: number
+  calorie: number
+  proteins: number
+  fats: number
+  increment: number
+  carbs: number
+}
